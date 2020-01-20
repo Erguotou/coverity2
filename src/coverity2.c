04da351656d3326b04ab2cab1cbeab9657f0b02b
Erguotou <jeromeliang@hotmail.com>
2020-01-20 11:38:16 -0800

    Add test functions

--- src/coverity2.c
+++ src/coverity2.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <unistd.h>
+
+void readTest() {
+  int fd = open("test.bin", O_RDONLY);
+  uint32_t id[2];
+  size_t size = read(fd, (void*) id, sizeof(id));
+  if (size == sizeof(id))
+    printf("%d, %d\n", id[0], id[1]);
+  if (fd != -1)
+      close(fd);
+}
 
 int main(void) {
+  readTest();
 	FILE *fd = fopen("test.bin", "rb");
 	int id[2];
 	size_t size = fread((void*) id, sizeof(int), 2, fd);
